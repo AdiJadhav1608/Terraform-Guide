ca733929dc5f1f8e90c66594f5b85a669873b609
AdiJadhav1608 <adijadhav8446@gmail.com>
2025-12-19 14:23:57 +0000

    add Terraform usage guide

---
+++ Topics/12_Terraform_Data_Sources/data.tf
@@ -0,0 +1,25 @@
+#====================================================
+#📄 **data.tf**
+#====================================================
+
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+#====================================================
