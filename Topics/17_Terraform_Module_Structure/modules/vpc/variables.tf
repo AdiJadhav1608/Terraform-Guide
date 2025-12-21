f99b4e1c6f9a1da7e84e25bb086a72367ea3b606
AdiJadhav1608 <adijadhav8446@gmail.com>
2025-12-21 16:55:58 +0000

    add Terraform usage guide

---
+++ Topics/17_Terraform_Module_Structure/modules/vpc/variables.tf
@@ -0,0 +1,12 @@
+#====================================================
+#📄 **modules/vpc/variables.tf**
+#===================================================
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+}
+#====================================================
