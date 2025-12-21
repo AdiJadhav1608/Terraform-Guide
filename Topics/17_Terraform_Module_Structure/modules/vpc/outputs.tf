8c41898e055d5ba5198018a5f878c7931c464270
AdiJadhav1608 <adijadhav8446@gmail.com>
2025-12-21 16:57:04 +0000

    add Terraform usage guide

---
+++ Topics/17_Terraform_Module_Structure/modules/vpc/outputs.tf
@@ -0,0 +1,14 @@
+#====================================================
+#📄 **modules/vpc/outputs.tf**
+#====================================================
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.this.id
+}
+
+output "vpc_cidr" {
+  description = "VPC CIDR block"
+  value       = aws_vpc.this.cidr_block
+}
+#====================================================
